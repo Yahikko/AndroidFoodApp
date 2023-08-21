946dc6e215335023f02629411de69d328cf15d91
Yahikko <taczuhiro@mail.ru>
2023-08-21 15:20:05 +0300

    setup retrofit; random meal functionality

--- app/src/main/java/com/example/androidfoodapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/androidfoodapp/activities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.androidfoodapp
+package com.example.androidfoodapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.androidfoodapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/example/androidfoodapp/fragments/HomeFragment.kt
@@ -0,0 +1,55 @@
+package com.example.androidfoodapp.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.androidfoodapp.R
+import com.example.androidfoodapp.databinding.FragmentHomeBinding
+import com.example.androidfoodapp.pojo.Meal
+import com.example.androidfoodapp.pojo.MealList
+import com.example.androidfoodapp.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if (response.body() != null) {
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Glide.with(this@HomeFragment)
+                        .load(randomMeal.strMealThumb)
+                        .into(binding.imgRandomMeal)
+                } else {
+                    return
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("HomeFragment", t.message.toString())
+            }
+        })
+    }
+}
